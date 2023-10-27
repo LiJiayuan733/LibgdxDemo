3da3a411cd422cd03a6d64cffe932da07be4e2df
LiJiayuan733 <76216471+LiJiayuan733@users.noreply.github.com>
2023-10-27 14:35:24 +0800

    Update Led1306Picture.java
    完善了一些

--- core/src/com/thzs/app/datacoplite/util/stm32utils/Led1306Picture.java
+++ core/src/com/thzs/app/datacoplite/util/stm32utils/Led1306Picture.java
@@ -6,13 +6,20 @@ import com.badlogic.gdx.graphics.Pixmap;
 
 public class Led1306Picture {
     public static final int PIC_CREATE_MODE_PAGE=0;
+    public static final int PIC_CREATE_MODE_HOR=1;
+    public static final int PIC_CREATE_MODE_VER=2;
     public static final int LED_WIDTH=128;
+    public static final int LED_HEIGHT=64;
+    public static final int LED_PAGE_NUM=8;
     public int picCreateMode=PIC_CREATE_MODE_PAGE;
     protected FileHandle picFile;
-    protected Pixmap pictureTemp;
+    protected Pixmap pictureTemp;                //原图存档
+    //TODO: need dispose
+    protected Pixmap pictureForWhiteAndBlack;    //黑白位图
     public Led1306Picture(FileHandle PicFile){
         this.picFile=PicFile;
         this.pictureTemp=new Pixmap(PicFile);
+        this.pictureForWhiteAndBlack=PicToWhiteAndBlack();
     }
     /**
      * 对图像二进制处理并生成对应的图像
@@ -20,26 +27,48 @@ public class Led1306Picture {
     public byte[] CreateData(){
         if(picCreateMode==PIC_CREATE_MODE_PAGE){
             return CreatePicturePage();
+        }else if(picCreateMode==PIC_CREATE_MODE_HOR){
+            return CreatePictureHor();
+        }else if(picCreateMode==PIC_CREATE_MODE_VER){
+            return CreatePictureVer();
         }
         return null;
     }
+    /**
+     * 产生页数据
+     */
     public byte[] CreatePicturePage(){
-        byte[] data=new byte[1024];
-        Pixmap pic=PicToWhiteAndBlack();
+        byte[] data=new byte[LED_WIDTH*LED_HEIGHT/8];
         int index=0;
         int y=0,x=0;
-        for(int page=0;page<8;page++){
+        for(int page=0;page<LED_PAGE_NUM;page++){
             y=page*8;
-            for(int col=0;col<128;col++){
+            for(int col=0;col<LED_WIDTH;col++){
                 int c=0;
-                for(int i=7;i>=0;i--){
+                for(int i=LED_PAGE_NUM-1;i>=0;i--){
                     c=c<<1;
-                    c+=PicPixelIsWhite(pic,col,y+i);
+                    c+=PicPixelIsWhite(pictureForWhiteAndBlack,col,y+i);
                 }
                 data[index]=(byte)c;
                 index++;
             }
         }
+        return data;
+    }
+    /**
+     * 产生横式数据
+     */
+    public byte[] CreatePictureHor(){
+        byte[] data=new byte[LED_WIDTH*LED_HEIGHT/8];
+        //TODO: to finish
+        return data;
+    }
+    /**
+     * 产生列式数据
+     */
+    public byte[] CreatePictureVer(){
+        byte[] data=new byte[LED_WIDTH*LED_HEIGHT/8];
+        //TODO: to finish
         return data;
     }
     /**
